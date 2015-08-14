43610af3affe34cc2d9f5ce9bc3b64a95de14ae2
Crocus <sam85@wp.pl>
2015-08-14 22:57:58 +0200

    Closed #1 - Support for non-string Id property.

--- src/DocumentDb.Repository/DocumentDbRepository.cs
+++ src/DocumentDb.Repository/DocumentDbRepository.cs
@@ -40,56 +40,7 @@ namespace DocumentDB.Repository
             _collectionName = collectionNameFactory != null ? collectionNameFactory() : typeof(T).Name;
 
             _repositoryIdentityProperty = TryGetIdProperty(idNameFactory);
-        }
-
-        private string TryGetIdProperty(Expression<Func<T, object>> idNameFactory)
-        {
-            Type entityType = typeof (T);            
-            var properties = entityType.GetProperties();
-
-            // search for idNameFactory
-            if (idNameFactory != null && idNameFactory.Body is MemberExpression)
-            {
-                MemberInfo customPropertyInfo = ((MemberExpression) idNameFactory.Body).Member;
-                EnsurePropertyHasJsonAttributeWithCorrectPropertyName(customPropertyInfo);
-
-                return customPropertyInfo.Name;
-            }
-
-            // search for id property in entity
-            var idProperty = properties.SingleOrDefault(p => p.Name == _defaultIdentityPropertyName);
-
-            if (idProperty != null)
-            {
-                return idProperty.Name;
-            }
-
-             // search for Id property in entity
-            idProperty = properties.SingleOrDefault(p => p.Name == "Id");
-
-            if (idProperty != null)
-            {
-                EnsurePropertyHasJsonAttributeWithCorrectPropertyName(idProperty);
-
-                return idProperty.Name;
-            }
-
-            // identity property not found;
-            throw new ArgumentException("Unique identity property not found. Create \"id\" property for your entity or use different property name with JsonAttribute with PropertyName set to \"id\"");
-        }
-
-        private void EnsurePropertyHasJsonAttributeWithCorrectPropertyName(MemberInfo idProperty)
-        {
-            var attributes = idProperty.GetCustomAttributes(typeof (JsonPropertyAttribute), true);
-            if (!(attributes.Length == 1 &&
-                ((JsonPropertyAttribute) attributes[0]).PropertyName == _defaultIdentityPropertyName))
-            {
-                throw new ArgumentException(
-                        string.Format(
-                            "\"{0}\" property needs to be decorated with JsonAttirbute with PropertyName set to \"id\"",
-                            idProperty.Name));
-            }
-        }
+        }  
 
         /// <summary>
         /// Removes the underlying DocumentDB collection. NOTE: Each time you create a collection, you incur a charge for at least one hour of use, as determined by the specified performance level of the collection. 
@@ -127,8 +78,10 @@ namespace DocumentDB.Repository
         {
             T upsertedEntity;
 
+            var entityId = GetId(entity);
+
             // check if entity exist
-            T existingEntity = await GetByIdAsync(GetId(entity));
+            T existingEntity = await GetByIdAsync(entityId);
 
             if (existingEntity != null)
             {
@@ -136,9 +89,7 @@ namespace DocumentDB.Repository
                 Document doc = await GetDocumentByIdAsync(GetId(existingEntity));
 
                 // update Id field if it doesn't exist
-                var entityId = GetId(entity);
-
-                if (string.IsNullOrEmpty(entityId))
+                if (entityId == null)
                 {
                     SetValue(_repositoryIdentityProperty, entity, GetId(existingEntity));
                 }
@@ -166,9 +117,9 @@ namespace DocumentDB.Repository
             return _client.CreateDocumentQuery<T>((await _collection).SelfLink).AsEnumerable();
         }
 
-        public async Task<T> GetByIdAsync(string id)
+        public async Task<T> GetByIdAsync(object id)
         {
-            return await FirstOrDefaultAsync(d => GetId(d) == id);
+            return await FirstOrDefaultAsync(d => GetId(d).Equals(id));
         }
 
         public async Task<T> FirstOrDefaultAsync(Func<T, bool> predicate)
@@ -191,9 +142,60 @@ namespace DocumentDB.Repository
             return _client.CreateDocumentQuery<T>((await _collection).DocumentsLink);
         }
 
-        private async Task<Document> GetDocumentByIdAsync(string id)
+        private string TryGetIdProperty(Expression<Func<T, object>> idNameFactory)
         {
-            return _client.CreateDocumentQuery<Document>((await _collection).SelfLink).Where(d => d.Id == id).AsEnumerable().FirstOrDefault();
+            Type entityType = typeof(T);
+            var properties = entityType.GetProperties();
+
+            // search for idNameFactory
+            if (idNameFactory != null)
+            {
+                var expr = GetMemberExpression(idNameFactory);
+                MemberInfo customPropertyInfo = expr.Member;
+
+                EnsurePropertyHasJsonAttributeWithCorrectPropertyName(customPropertyInfo);
+
+                return customPropertyInfo.Name;
+            }
+
+            // search for id property in entity
+            var idProperty = properties.SingleOrDefault(p => p.Name == _defaultIdentityPropertyName);
+
+            if (idProperty != null)
+            {
+                return idProperty.Name;
+            }
+
+            // search for Id property in entity
+            idProperty = properties.SingleOrDefault(p => p.Name == "Id");
+
+            if (idProperty != null)
+            {
+                EnsurePropertyHasJsonAttributeWithCorrectPropertyName(idProperty);
+
+                return idProperty.Name;
+            }
+
+            // identity property not found;
+            throw new ArgumentException("Unique identity property not found. Create \"id\" property for your entity or use different property name with JsonAttribute with PropertyName set to \"id\"");
+        }
+
+        private void EnsurePropertyHasJsonAttributeWithCorrectPropertyName(MemberInfo idProperty)
+        {
+            var attributes = idProperty.GetCustomAttributes(typeof(JsonPropertyAttribute), true);
+            if (!(attributes.Length == 1 &&
+                ((JsonPropertyAttribute)attributes[0]).PropertyName == _defaultIdentityPropertyName))
+            {
+                throw new ArgumentException(
+                        string.Format(
+                            "\"{0}\" property needs to be decorated with JsonAttirbute with PropertyName set to \"id\"",
+                            idProperty.Name));
+            }
+        }
+
+        private async Task<Document> GetDocumentByIdAsync(object id)
+        {
+            return _client.CreateDocumentQuery<Document>((await _collection).SelfLink).Where(d => d.Id == id.ToString()).AsEnumerable().FirstOrDefault();
         }
 
         private async Task<DocumentCollection> GetOrCreateCollectionAsync()
@@ -223,11 +225,15 @@ namespace DocumentDB.Repository
             return database;
         }
 
-        private string GetId(T entity)
+        private object GetId(T entity)
         {
             var p = Expression.Parameter(typeof(T), "x");
-            var body = Expression.Property(p, _repositoryIdentityProperty);
-            var exp = Expression.Lambda<Func<T, string>>(body, p);
+            Expression body = Expression.Property(p, _repositoryIdentityProperty);
+            if (body.Type.IsValueType)
+            {
+                body = Expression.Convert(body, typeof(object));
+            }
+            var exp = Expression.Lambda<Func<T, object>>(body, p);
             return exp.Compile()(entity);
         }
 
@@ -236,6 +242,13 @@ namespace DocumentDB.Repository
             MethodInfo method = typeof(T).GetProperty(propertyName).GetSetMethod();
             Action<T, TV> setter = (Action<T, TV>)Delegate.CreateDelegate(typeof(Action<T, TV>), method);
             setter(item, value);
+        }
+
+        private MemberExpression GetMemberExpression<T>(Expression<Func<T, object>> expr)
+        {
+            var member = expr.Body as MemberExpression;
+            var unary = expr.Body as UnaryExpression;
+            return member ?? (unary != null ? unary.Operand as MemberExpression : null);
         }
     }
 }
